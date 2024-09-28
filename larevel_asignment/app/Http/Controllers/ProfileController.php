47b8f0a602539fdfcb249c2fdeea06b38242eba9
Mainul Kanon <mainul5770@gmail.com>
2024-09-28 22:43:23 +0600

    asignment

---
+++ larevel_asignment/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+
+        $name = "Donal Trump";
+        $age = "75";
+
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+
+        $cookie_name = 'access_token';
+        $cookie_value = '123-XYZ';
+        $cookie_minutes = 1;
+        $cookie_path = '/';
+        $cookie_domain = $_SERVER['SERVER_NAME'];
+        $cookie_secure = false;
+        $cookie_httpOnly = true;
+
+
+        return response()->json($data, 200)
+            ->cookie($cookie_name, $cookie_value, $cookie_minutes, $cookie_path, $cookie_domain, $cookie_secure, $cookie_httpOnly);
+    }
+}
